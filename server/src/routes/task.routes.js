0b6dedc7bf3fc4af89a064d19041abbb9d450cbb
Osk <oscar.martin@alu.ceacfp.es>
2026-03-23 01:10:40 +0100

    feat: fase 3 completada - arquitectura fullstack inexpugnable y despliegue live

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -1,11 +1,22 @@
-// server/src/routes/task.routes.js
+/**
+ * TASK ROUTES - Mapa de Acceso del Gremio
+ * Define los puntos de entrada legales para las operaciones de misiones.
+ */
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/task.controller');
 
+// [GET] Obtener listado de misiones
 router.get('/', taskController.getTasks);
+
+// [POST] Publicar un nuevo contrato
 router.post('/', taskController.createTask);
-router.delete('/:id', taskController.deleteTask);
+
+// [PATCH] Actualizar estado de una misión (Completar/Reabrir)
+// Es vital que el ID sea dinámico (:id) para localizar el objetivo
 router.patch('/:id', taskController.updateTaskStatus);
+
+// [DELETE] Eliminar registro permanentemente
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
